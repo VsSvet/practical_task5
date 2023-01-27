4ccf3e483848f0ec121d570ecdfdb8373c61823a
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-27 12:25:40 +0300

    add Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int size = 4;
+int[] array = GetArray(size);
+WriteLine(String.Join(",", array));
+int Sum = 0;
+
+for(int i = 0; i < size; i++)
+{
+    if(i % 2 != 0)
+    {
+        Sum += array[i];
+    }
+    
+}
+
+
+WriteLine($"Сумма элементов, стоящих на нечётных позициях = {Sum}");
+
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(-100, 100);
+    }
+    return result;
+}
