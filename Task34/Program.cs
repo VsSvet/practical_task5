f3c453410a4c1f20f07b4f74975020ee802d08b0
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-27 12:06:45 +0300

    add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int[] array = GetArray(4);
+WriteLine(String.Join(",", array));
+int Sum = 0;
+foreach(int item in array)
+{
+    if(item % 2 == 0)
+    {
+        Sum++;
+    }
+    
+}
+
+
+WriteLine($"Количество четных чисел = {Sum}");
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
